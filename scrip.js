c353ace9bbdc6b369add098631cb39a21b4d12f5
Jortegla <jortegla@gmail.com>
2023-04-30 20:02:44 -0300

    Estilos del encriptador

--- scrip.js
+++
@@ -1,9 +0,0 @@
-function encriptar(){
-    var mensaje = document.getElementById("textoEncriptado").value.tolowerCase();
-
-    var mensajeEncriptado = mensaje.replace(/e/img, "enter");
-    var mensajeEncriptado = mensaje.replace(/i/img, "imes");
-    var mensajeEncriptado = mensaje.replace(/a/img, "ai");
-    var mensajeEncriptado = mensaje.replace(/o/img, "ober");
-    var mensajeEncriptado = mensaje.replace(/u/img, "ufat");
-}
